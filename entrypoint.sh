35410692d45708dfca0b04be4e427193888ea441
javatalent <kushalnauhwar@gmail.com>
2022-03-17 15:10:25 +0100

    new split function

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
+
 #!/bin/sh
 
 set -e
@@ -21,8 +22,12 @@ then
     echo "commit_msg must be defined"
     return 2
 fi
-IFS=',' read -ra source_folders <<< "$INPUT_SOURCE_FOLDER"
-IFS=',' read -ra destination_folders <<< "$INPUT_DESTINATION_FOLDER"
+
+set -f
+IFS=,
+source_folders=($INPUT_SOURCE_FOLDER)
+destination_folders=($INPUT_DESTINATION_FOLDER)
+
 source_folders_len=${#source_folders[@]}
 destination_folders_len=${#destination_folders[@]}
 if [[ $source_folders_len -ne $destination_folders_len ]]; then
